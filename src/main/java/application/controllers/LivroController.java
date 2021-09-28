f454e9fc7e4c1f97151782b45f14a99c0291c071
JoaodeSouza00 <joaovitor102475@gmail.com>
2021-09-28 19:17:23 +0000

    arrumando o erro

--- src/main/java/application/controllers/LivroController.java
+++ src/main/java/application/controllers/LivroController.java
@@ -1,17 +1,33 @@
 package application.controllers;
 
-import org.springframework.beans.factory.annotation.Autowired;
+
+
+//import org.springframework.beans.factory.annotation.Autowired;
+
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.RequestMapping;
+
+//import org.springframework.ui.Model;
+
+//import org.springframework.web.bind.annotation.RequestMapping;
+
+
 
 @Controller
-@RequestMapping("/livro")
+
+//@RequestMapping("/livro")
+
 public class LivroController{
-    @Autowired
+
+   /* @Autowired
+
     @RequestMapping("/list")
+
     public String list(Model model) {
+
         model.addAttribute("livros");
+
         return "list.jsp";
-    }
+
+    }*/
+
 }
